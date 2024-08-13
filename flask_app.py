7c8df225e100e9023107216abc49b7596d709abc
Colin-Prim <cprim@umich.edu>
2024-08-13 13:32:51 -0400

    take 2

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return '''<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pose Estimator</title>
+</head>
+<body>
+   <form action = "/success" method = "post" enctype="multipart/form-data">
+        <input type="file" name="file" />
+        <input type = "submit" value="Upload">
+    </form>
+</body>
+</html>'''
 
 
 if __name__ == '__main__':

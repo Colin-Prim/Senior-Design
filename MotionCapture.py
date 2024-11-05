3a373f057204632d541468900f23ab78845fb0df
JSM177Y <jsmittee7@gmail.com>
2024-11-05 17:04:24 -0500

    this is the new code for now. it is a giant work in progress, but I gotta leave for the night

--- MotionCapture.py
+++ MotionCapture.py
@@ -9,3 +9,7 @@ with h5py.File('annot_coco.h5', 'w') as h5file:
     # Assuming you want to save keypoints
     keypoints = [ann['keypoints'] for ann in coco.loadAnns(coco.getAnnIds()) if 'keypoints' in ann]
     h5file.create_dataset('part', data=keypoints)
+
+
+
+#currently using this to run the training models, the "Main" is in videopose.py for now
